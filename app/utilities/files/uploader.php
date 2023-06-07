6aabda8a6d68c387f7e8c5d9db92aa38bb60b0f6
Denniel <sadiandenniel@gmail.com>
2023-06-07 17:43:15 +0800

    Create `uploader.php`

---
+++ app/utilities/files/uploader.php
@@ -0,0 +1,14 @@
+<?php
+use Ramsey\Uuid\Uuid;
+
+
+function save($file) {
+    $target_dir = '/var/www/media/uploads/';
+    if (!file_exists($target_dir)) {
+        mkdir($target_dir, 0777, true);
+    }
+    $target_file = $target_dir . Uuid::uuid4()->toString() . '-' .  basename($file["name"]);
+    move_uploaded_file($file["tmp_name"], $target_file);
+    $rel_path = str_replace('/var/www/', '', $target_file);
+    return $rel_path;
+}
