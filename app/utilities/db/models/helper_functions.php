bbd42e625a0a39a9e2fedc0d6cb8b9c81e19605c
Denniel <sadiandenniel@gmail.com>
2023-06-11 00:27:08 +0800

    Update the helper functions

--- app/utilities/db/models/helper_functions.php
+++ app/utilities/db/models/helper_functions.php
@@ -1,22 +1,30 @@
 <?php
-function create_where_clause_for_search($params) {
+function create_where_clause_for_search($params, $is_strict) {
     if (count($params) != 0) {
         $clauses = [];
         foreach ($params as $field => $str_search) {
-            array_push($clauses, "LOWER($field) LIKE :$field");
+            if ($is_strict) {
+                array_push($clauses, "$field = :$field");
+            } else {
+                array_push($clauses, "LOWER($field) LIKE :$field");
+            }
         }
-        $where = ' WHERE '.implode(', ', $clauses);
+        $where = ' WHERE '.implode(' and ', $clauses);
         return $where;
     }
     return '';
 }
 
 
-function create_params_for_search($params) {
+function create_params_for_search($params, $is_strict) {
     if (count($params) != 0) {
         $modified_params = [];
-        foreach ($params as $field => $str_search) {
-            $modified_params[$field] = "%$str_search%";
+        foreach ($params as $field => $search) {
+            if ($is_strict) {
+                $modified_params[$field] = $search;
+            } else {
+                $modified_params[$field] = "%$search%";
+            }
         }
         return $modified_params;
     }
