41f834b231895ec631a36f792da6995b31b49d5c
Denniel <sadiandenniel@gmail.com>
2023-06-11 19:59:25 +0800

    Create `login.php`

---
+++ app/login.php
@@ -0,0 +1,18 @@
+<?php
+require_once '/var/www/utilities/twig.php';
+require_once '/var/www/utilities/auth/session.php';
+require_once '/var/www/utilities/auth/guards.php';
+
+$context = array();
+
+if (isset($_POST['submit'])) {
+    $user = login($_POST['username'], $_POST['password']);
+
+    if ($user != null) {
+        redirect();
+    } else {
+        $context['error'] = 'Ooops! You entered an invalid username or password.';
+    }
+}
+
+echo $twig->render('login.html', $context);
