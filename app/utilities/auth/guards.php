f18c20cfa1aae2fd51509b1851361b2b4bd8f111
Denniel <sadiandenniel@gmail.com>
2023-06-11 20:03:02 +0800

    Create `guards.php`

---
+++ app/utilities/auth/guards.php
@@ -0,0 +1,25 @@
+<?php
+require_once '/var/www/utilities/auth/session.php';
+
+
+function allow($types) {
+    if (isset($_SESSION['user'])) {
+        $user = unserialize($_SESSION['user']);
+        if (!in_array($user->type, $types)) {
+            exit(header('Location: /forbidden.php'));
+        }
+    } else {
+        exit(header('Location: /login.php'));
+    }
+}
+
+function redirect() {
+    if (isset($_SESSION['user'])) {
+        $user = unserialize($_SESSION['user']);
+        if ($user->type == Models\UserType::CLIENT) {
+
+        } elseif ($user->type == Models\UserType::ADMIN) {
+            exit(header('Location: /admin/butterflies/list.php'));
+        }
+    }
+}
