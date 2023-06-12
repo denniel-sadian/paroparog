818a150b195927522db6ffc442fd26bf5f9a00af
Denniel <sadiandenniel@gmail.com>
2023-06-13 07:55:34 +0800

    Create `details.php`

---
+++ app/profile/details.php
@@ -0,0 +1,31 @@
+<?php
+require_once '/var/www/utilities/twig.php';
+require_once '/var/www/utilities/context.php';
+require_once '/var/www/utilities/auth/guards.php';
+require_once '/var/www/utilities/db/models/users.php';
+require_once '/var/www/utilities/db/models/wfp_wcp.php';
+require_once '/var/www/utilities/files/uploader.php';
+
+use Models\UserType;
+use Models\WfpWcp;
+
+allow(UserType::ALL);
+
+
+if (isset($_POST['submit'])) {
+    $user = unserialize($_SESSION['user']);
+    $user->username = $_POST['username'];
+    $user->first_name = $_POST['first_name'];
+    $user->last_name = $_POST['last_name'];
+    $user->gender = $_POST['gender'];
+    $user->email = $_POST['email'];
+    $user->save();
+    $_SESSION['user'] = serialize($user);
+
+    exit(header('Location: /admin/wfpwcp/list.php'));
+}
+
+
+$CONTEXT['tab'] = 'profile';
+
+echo $twig->render('profile_details.html', $CONTEXT);
