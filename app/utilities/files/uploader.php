17d1e694d0dabf760d35112848cc5726857b3dbd
Denniel <sadiandenniel@gmail.com>
2023-06-13 07:27:45 +0800

    Update `save` method

--- app/utilities/files/uploader.php
+++ app/utilities/files/uploader.php
@@ -3,12 +3,15 @@ use Ramsey\Uuid\Uuid;
 
 
 function save($file) {
-    $target_dir = '/var/www/media/uploads/';
-    if (!file_exists($target_dir)) {
-        mkdir($target_dir, 0777, true);
+    if ($file['size'] > 0) {
+        $target_dir = '/var/www/media/uploads/';
+        if (!file_exists($target_dir)) {
+            mkdir($target_dir, 0777, true);
+        }
+        $target_file = $target_dir . Uuid::uuid4()->toString() . '-' .  basename($file["name"]);
+        move_uploaded_file($file["tmp_name"], $target_file);
+        $rel_path = str_replace('/var/www', '', $target_file);
+        return $rel_path;
     }
-    $target_file = $target_dir . Uuid::uuid4()->toString() . '-' .  basename($file["name"]);
-    move_uploaded_file($file["tmp_name"], $target_file);
-    $rel_path = str_replace('/var/www/', '', $target_file);
-    return $rel_path;
+    return null;
 }
