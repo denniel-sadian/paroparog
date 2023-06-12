1abc03d91d277c15c701d4208f6c0b3966178eee
Denniel <sadiandenniel@gmail.com>
2023-06-12 17:40:28 +0800

    Create `update_password`

---
+++ app/profile/update_password.php
@@ -0,0 +1,22 @@
+<?php
+require_once '/var/www/utilities/twig.php';
+require_once '/var/www/utilities/context.php';
+require_once '/var/www/utilities/auth/guards.php';
+require_once '/var/www/utilities/db/models/users.php';
+
+
+$CONTEXT['tab'] = 'profile';
+
+if (isset($_POST['submit']) && isset($_SESSION['user'])) {
+    $user = unserialize($_SESSION['user']);
+    $password = $_POST['password'];
+    $user->set_password($password);
+    $user->password_changed = 'shit';
+    $user->save();
+
+    $_SESSION['user'] = serialize($user);
+
+    redirect();
+}
+
+echo $twig->render('update_password.html', $CONTEXT);
