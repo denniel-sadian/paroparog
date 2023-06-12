f167a0b2e6567e3649395990923d8a093cd66a48
Denniel <sadiandenniel@gmail.com>
2023-06-12 17:41:04 +0800

    Create `create.php`

---
+++ app/admin/users/create.php
@@ -0,0 +1,39 @@
+<?php
+require_once '/var/www/utilities/twig.php';
+require_once '/var/www/utilities/context.php';
+require_once '/var/www/utilities/auth/guards.php';
+require_once '/var/www/utilities/db/models/users.php';
+require_once '/var/www/utilities/db/models/dtos.php';
+require_once '/var/www/utilities/messaging/mailer.php';
+
+use Ramsey\Uuid\Uuid;
+use Models\UserType;
+use Models\User;
+use DTOs\Search;
+use DTOs\Sort;
+use DTOs\PageRequest;
+
+allow([UserType::ADMIN]);
+
+
+if (isset($_POST['submit'])) {
+    $password = strtolower(str_replace('-', '', Uuid::uuid4()->toString()));
+    $_POST['password'] = $password;
+    $_POST['active'] = false;
+    $user = User::create($_POST);
+    $user->save();
+
+    $message = "
+    User Type: $user->type\n
+    Username: $user->username\n
+    Temporary Password: $password
+    ";
+    send_mail($user->email, $message);
+
+    exit(header('Location: /admin/users/list.php'));
+}
+
+
+$CONTEXT['tab'] = 'users';
+
+echo $twig->render('user_create.html', $CONTEXT);
