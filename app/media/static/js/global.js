8bf01332d8707d459aea40f4be926115489a7c81
Denniel <sadiandenniel@gmail.com>
2023-06-13 10:35:31 +0800

    Create `global.js`

---
+++ app/media/static/js/global.js
@@ -0,0 +1,28 @@
+function buildUsername() {
+    const uType = $('#type').val();
+    const fn = $('#fn').val().replace(' ', '').toLowerCase();
+    const ln = $('#ln').val().replace(' ', '').toLowerCase();
+    console.log(uType);
+    let username = uType == 'CLIENT' ? 'ltpmdq_' : '';
+    username += fn.substring(0, 3) + ln;
+    $('.username').val(username.toLowerCase());
+}
+
+$(function () {
+    [...document.querySelectorAll('a')].forEach(e => {
+        const url = new URL(e.href);
+        for (let [k, v] of new URLSearchParams(window.location.search).entries()) {
+            if (url.searchParams.get(k) == null) {
+                url.searchParams.set(k, v);
+            }
+        }
+        e.href = url.toString();
+    });
+
+    $('#fn').keydown(buildUsername);
+    $('#ln').keydown(buildUsername);
+    $('#fn').keyup(buildUsername);
+    $('#ln').keyup(buildUsername);
+    $('#type').change(buildUsername);
+    buildUsername();
+});
