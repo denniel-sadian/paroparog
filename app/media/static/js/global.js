2f41000d00ae78c28b3ff4ae179ed634c5beb7f1
Denniel <sadiandenniel@gmail.com>
2023-08-07 00:07:50 +0800

    Make some pages responsive

--- app/media/static/js/global.js
+++ app/media/static/js/global.js
@@ -1,17 +1,23 @@
 function buildUsername() {
-    const uType = $('#type').val();
-    const fn = $('#fn').val().replace(' ', '').toLowerCase();
-    const ln = $('#ln').val().replace(' ', '').toLowerCase();
+    const uType = $("#type").val();
+    const fn = $("#fn").val().replace(" ", "").toLowerCase();
+    const ln = $("#ln").val().replace(" ", "").toLowerCase();
     console.log(uType);
-    let username = uType == 'CLIENT' ? 'ltpmdq_' : '';
+    let username = uType == "CLIENT" ? "ltpmdq_" : "";
     username += fn.substring(0, 3) + ln;
-    $('.username').val(username.toLowerCase());
+    $(".username").val(username.toLowerCase());
 }
 
 $(function () {
-    [...document.querySelectorAll('a')].forEach(e => {
+    $("#nav-btn").click(function () {
+        $("#nav-links").toggle();
+    });
+
+    [...document.querySelectorAll("a")].forEach((e) => {
         const url = new URL(e.href);
-        for (let [k, v] of new URLSearchParams(window.location.search).entries()) {
+        for (let [k, v] of new URLSearchParams(
+            window.location.search
+        ).entries()) {
             if (url.searchParams.get(k) == null) {
                 url.searchParams.set(k, v);
             }
@@ -19,10 +25,10 @@ $(function () {
         e.href = url.toString();
     });
 
-    $('#fn').keydown(buildUsername);
-    $('#ln').keydown(buildUsername);
-    $('#fn').keyup(buildUsername);
-    $('#ln').keyup(buildUsername);
-    $('#type').change(buildUsername);
+    $("#fn").keydown(buildUsername);
+    $("#ln").keydown(buildUsername);
+    $("#fn").keyup(buildUsername);
+    $("#ln").keyup(buildUsername);
+    $("#type").change(buildUsername);
     buildUsername();
 });
